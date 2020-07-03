bfcf62c10bdb8ccd3316872ddc50357150912b32
FARFETCH\douglas.lima <douglas.lima@farfetch.com>
2020-07-03 18:35:53 +0100

    docs: add documentation ProtobufMessageSerializer class

--- src/KafkaFlow.Serializer.ProtoBuf/ProtobufMessageSerializer.cs
+++ src/KafkaFlow.Serializer.ProtoBuf/ProtobufMessageSerializer.cs
@@ -4,24 +4,33 @@
     using System.IO;
     using global::ProtoBuf;
 
-    public class ProtobufMessageSerializer : IMessageSerializer
+    /// <summary>
+    /// Serializer class for messages using protobuf protocol
+    /// </summary>
+    public abstract class ProtobufMessageSerializer : IMessageSerializer
     {
-        public byte[] Serialize(object obj)
+        /// <summary>
+        /// Creates a protocol-buffer representation of the given instance
+        /// </summary>
+        /// <param name="message">The message to be serialized (cannot be null)</param>
+        /// <returns>The serialized message</returns>
+        public byte[] Serialize(object message)
         {
-            using (var stream = new MemoryStream())
-            {
-                Serializer.Serialize(stream, obj);
-
-                return stream.ToArray();
-            }
+            using var stream = new MemoryStream();
+            Serializer.Serialize(stream, message);
+            return stream.ToArray();
         }
 
+        /// <summary>
+        /// Creates a new instance from a protocol-buffer representation
+        /// </summary>
+        /// <param name="data">The message to be deserialized (cannot be null)</param>
+        /// <param name="type">The type to be created</param>
+        /// <returns>The deserialized message</returns>
         public object Deserialize(byte[] data, Type type)
         {
-            using (var stream = new MemoryStream(data))
-            {
-                return Serializer.Deserialize(type, stream);
-            }
+            using var stream = new MemoryStream(data);
+            return Serializer.Deserialize(type, stream);
         }
     }
 }
