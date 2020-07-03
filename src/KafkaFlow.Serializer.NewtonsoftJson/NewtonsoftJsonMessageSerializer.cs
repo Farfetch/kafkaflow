8ad3daa27ec92832879b29c9e3edcf9b78048255
FARFETCH\douglas.lima <douglas.lima@farfetch.com>
2020-07-03 18:27:46 +0100

    docs: add documentation for NewtonsoftJsonMessageSerializer class

--- src/KafkaFlow.Serializer.NewtonsoftJson/NewtonsoftJsonMessageSerializer.cs
+++ src/KafkaFlow.Serializer.NewtonsoftJson/NewtonsoftJsonMessageSerializer.cs
@@ -4,26 +4,43 @@
     using System.Text;
     using Newtonsoft.Json;
 
+    /// <summary>
+    /// Serializer class for messages using json protocol (using Newtonsoft library)
+    /// </summary>
     public class NewtonsoftJsonMessageSerializer : IMessageSerializer
     {
         private readonly JsonSerializerSettings settings;
 
+        /// <summary>
+        /// NewtonsoftJsonMessageSerializer constructor  
+        /// </summary>
+        /// <param name="settings">Json serializer settings</param>
         public NewtonsoftJsonMessageSerializer(JsonSerializerSettings settings)
         {
             this.settings = settings;
         }
 
+        /// <summary>
+        /// NewtonsoftJsonMessageSerializer constructor  
+        /// </summary>
         public NewtonsoftJsonMessageSerializer()
             : this(new JsonSerializerSettings())
         {
         }
 
-        public byte[] Serialize(object obj)
+        /// <summary>Creates a json representation of the given instance</summary>
+        /// <param name="message">The message to be serialized (cannot be null)</param>
+        /// <returns>The serialized message</returns>
+        public byte[] Serialize(object message)
         {
-            var serialized = JsonConvert.SerializeObject(obj, this.settings);
+            var serialized = JsonConvert.SerializeObject(message, this.settings);
             return Encoding.UTF8.GetBytes(serialized);
         }
 
+        /// <summary>Creates a new instance from a json representation</summary>
+        /// <param name="data">The message to be deserialized (cannot be null)</param>
+        /// <param name="type">The type to be created.</param>
+        /// <returns>The deserialized message</returns>
         public object Deserialize(byte[] data, Type type)
         {
             var json = Encoding.UTF8.GetString(data);
