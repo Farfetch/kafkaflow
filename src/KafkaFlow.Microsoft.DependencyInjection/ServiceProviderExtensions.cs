8daeef71acdf84222c18f99e27aef1f9e53aac52
FARFETCH\douglas.lima <douglas.lima@farfetch.com>
2020-07-03 18:02:35 +0100

    docs: add documentation for KafkaFlow.Microsoft.DependencyInjection files

--- src/KafkaFlow.Microsoft.DependencyInjection/ServiceProviderExtensions.cs
+++ src/KafkaFlow.Microsoft.DependencyInjection/ServiceProviderExtensions.cs
@@ -5,9 +5,15 @@ namespace KafkaFlow
     using KafkaFlow.Configuration;
 
     /// <summary>
+    /// Extension methods over IServiceProvider
     /// </summary>
     public static class ServiceProviderExtensions
     {
+        /// <summary>
+        /// Creates a kafka bus using a instance of KafkaFlowConfigurator
+        /// </summary>
+        /// <param name="provider">Instance of <see cref="IServiceProvider"/></param>
+        /// <returns><see cref="IKafkaBus"/> created</returns>
         public static IKafkaBus CreateKafkaBus(this IServiceProvider provider)
         {
             var resolver = provider.GetRequiredService<IDependencyResolver>();
