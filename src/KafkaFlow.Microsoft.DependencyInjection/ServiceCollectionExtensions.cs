e0315354772fc018ce86b4c1c6858246b2bf0e2c
FARFETCH\douglas.lima <douglas.lima@farfetch.com>
2020-07-03 18:02:35 +0100

    docs: add documentation for KafkaFlow.Microsoft.DependencyInjection files

--- src/KafkaFlow.Microsoft.DependencyInjection/ServiceCollectionExtensions.cs
+++ src/KafkaFlow.Microsoft.DependencyInjection/ServiceCollectionExtensions.cs
@@ -6,9 +6,16 @@ namespace KafkaFlow
     using KafkaFlow.Microsoft.DependencyInjection;
 
     /// <summary>
+    /// Extension methods over IServiceCollection
     /// </summary>
     public static class ServiceCollectionExtensions
     {
+        /// <summary>
+        /// Adds a singleton instance of <see cref="KafkaFlowConfigurator"/> with <see cref="MicrosoftDependencyConfigurator"/>
+        /// </summary>
+        /// <param name="services">Instance of <see cref="IServiceCollection"/></param>
+        /// <param name="kafka">Action to be done over instance of <see cref="IKafkaConfigurationBuilder"/></param>
+        /// <returns></returns>
         public static IServiceCollection AddKafka(
             this IServiceCollection services,
             Action<IKafkaConfigurationBuilder> kafka)
