b1ad9fbfdf0e2df99af1e8326a5dddd7016ae34f
Filipe Esch <filipe.esch@farfetch.com>
2020-07-06 17:14:07 +0100

    refactor: remove .net fw samples and unify Producer and Consumer samples

--- samples/KafkaFlow.Sample/Program.cs
+++ samples/KafkaFlow.Sample/Program.cs
@@ -1,31 +1,36 @@
-﻿namespace KafkaFlow.Samples.NetFramework.Consumer
+﻿namespace KafkaFlow.Sample
 {
     using System;
-    using global::Unity;
+    using global::Microsoft.Extensions.DependencyInjection;
     using KafkaFlow.Compressor;
     using KafkaFlow.Compressor.Gzip;
-    using KafkaFlow.Configuration;
     using KafkaFlow.Consumers;
-    using KafkaFlow.Samples.Common;
     using KafkaFlow.Serializer;
-    using KafkaFlow.Serializer.Json;
     using KafkaFlow.Serializer.ProtoBuf;
     using KafkaFlow.TypedHandler;
-    using KafkaFlow.Unity;
 
-    class Program
+    internal static class Program
     {
-        static void Main(string[] args)
+        private static void Main()
         {
-            var container = new UnityContainer();
+            var services = new ServiceCollection();
 
-            var configurator = new KafkaFlowConfigurator(
-                new UnityDependencyConfigurator(container),
+            services.AddKafka(
                 kafka => kafka
                     .UseLogHandler<ConsoleLogHandler>()
                     .AddCluster(
                         cluster => cluster
                             .WithBrokers(new[] { "localhost:9092" })
+                            .AddProducer<PrintConsoleProducer>(
+                                producer => producer
+                                    .DefaultTopic("test-topic")
+                                    .AddMiddlewares(
+                                        middlewares => middlewares
+                                            .AddSerializer<ProtobufMessageSerializer, SampleMessageTypeResolver>()
+                                            .AddCompressor<GzipMessageCompressor>()
+                                    )
+                                    .WithAcks(Acks.All)
+                            )
                             .AddConsumer(
                                 consumer => consumer
                                     .Topic("test-topic")
@@ -39,59 +44,59 @@
                                             .AddSerializer<ProtobufMessageSerializer, SampleMessageTypeResolver>()
                                             .AddTypedHandlers(
                                                 handlers => handlers
-                                                    .WithHandlerLifetime(global::KafkaFlow.InstanceLifetime.Singleton)
-                                                    .AddHandler<PrintConsoleHandler>())
-                                    )
-                            )
-                            .AddConsumer(
-                                consumer => consumer
-                                    .Topic("test-topic-json")
-                                    .WithGroupId("print-console-handler")
-                                    .WithBufferSize(100)
-                                    .WithWorkersCount(10)
-                                    .WithAutoOffsetReset(AutoOffsetReset.Latest)
-                                    .AddMiddlewares(
-                                        middlewares => middlewares
-                                            .AddCompressor<GzipMessageCompressor>()
-                                            .AddSerializer<JsonMessageSerializer, SampleMessageTypeResolver>()
-                                            .AddTypedHandlers(
-                                                handlers => handlers
-                                                    .WithHandlerLifetime(global::KafkaFlow.InstanceLifetime.Singleton)
+                                                    .WithHandlerLifetime(InstanceLifetime.Singleton)
                                                     .AddHandler<PrintConsoleHandler>())
                                     )
                             )
                     )
             );
 
-            var bus = configurator.CreateBus(new UnityDependencyResolver(container));
+            services.AddSingleton<PrintConsoleProducer>();
+
+            var provider = services.BuildServiceProvider();
+
+            var bus = provider.CreateKafkaBus();
 
             bus.StartAsync().GetAwaiter().GetResult();
 
-            var consumerAcessor = container.Resolve<IConsumerAccessor>();
+            var printConsole = provider.GetService<PrintConsoleProducer>();
+            var consumerAcessor = provider.GetRequiredService<IConsumerAccessor>();
 
             while (true)
             {
-                var input = Console.ReadLine();
+                Console.Write("Number of messages to produce, Pause, Resume, or Exit:");
+                var input = Console.ReadLine().ToLower();
 
                 switch (input)
                 {
-                    case "pause":
+                    case var _ when int.TryParse(input, out var count):
+                        for (var i = 0; i < count; i++)
+                        {
+                            printConsole.Produce(new TestMessage { Text = $"Message: {Guid.NewGuid()}" });
+                        }
 
+                        break;
+
+                    case "pause":
                         foreach (var consumer in consumerAcessor.All)
                         {
                             consumer.Pause(consumer.Assignment);
                         }
 
+                        Console.WriteLine("Consumer paused");
+
                         break;
 
                     case "resume":
-
                         foreach (var consumer in consumerAcessor.All)
                         {
                             consumer.Resume(consumer.Assignment);
                         }
 
+                        Console.WriteLine("Consumer resumed");
+
                         break;
+
                     case "exit":
                         return;
                 }
