43db13fca2ec386745f4f8753e7c6d5e665b9d25
Felipe Lino <felipe.lino@farfetch.com>
2020-09-21 13:53:52 +0100

    feat: add serializer protobuf for google lib

---
+++ src/KafkaFlow.UnitTests/Serializers/GoogleProtobufMessageSerializerTests.cs
@@ -0,0 +1,80 @@
+﻿namespace KafkaFlow.UnitTests.Serializers
+{
+    using Google.Protobuf.Examples.AddressBook;
+    using KafkaFlow.Serializer.GoogleProtobuf;
+    using KafkaFlow.UnitTests.Serializers.Resources;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class GoogleProtobufMessageSerializerTests
+    {
+        private GoogleProtobufMessageSerializer serializer = new GoogleProtobufMessageSerializer();
+        
+        [TestMethod]
+        public void SerializeAndDeserialize_WhenTheClassIsFromGoogleCompiler_ShouldProduceEqualsObjects()
+        {
+            object msg1 = CreateProtobufGoogleObject();
+            
+            // Act - 1
+            var byteArray = serializer.Serialize(msg1);
+            
+            // Assert
+            Assert.IsNotNull(byteArray);
+
+            object msg2 = serializer.Deserialize(byteArray, typeof(Person));
+            
+            // Assert
+            Assert.IsNotNull(msg2);
+            Assert.AreNotSame(msg1, msg2);
+            Assert.AreEqual(msg1, msg2);
+        }
+        
+        [TestMethod]
+        public void SerializeAndDeserialize_WhenTheClassIsFromProtobufNet_ShouldProduceEqualsObjects()
+        {
+            object msg1 = CreateProtobufNetObject();
+            
+            // Act - 1
+            var byteArray = serializer.Serialize(msg1);
+            
+            // Assert
+            Assert.IsNotNull(byteArray);
+
+            object msg2 = serializer.Deserialize(byteArray, typeof(PersonFromProtobufNet));
+            
+            // Assert
+            Assert.IsNotNull(msg2);
+            Assert.AreNotSame(msg1, msg2);
+            Assert.AreEqual(msg1, msg2);
+        }
+
+        private static object CreateProtobufGoogleObject()
+        {
+            /* The class Person was created using the addressbook.proto and the generated class Addressbook.cs
+             using the compiler provided by Google, following the instructions from here: https://developers.google.com/protocol-buffers/docs/csharptutorial
+             */
+            object john = new Person
+            {
+                Id = 1234,
+                Name = "John Doe",
+                Email = "jdoe@example.com",
+                Phones = { new Person.Types.PhoneNumber { Number = "555-4321", Type = Person.Types.PhoneType.Home } }
+            };
+
+            return john;
+        }
+
+        private static object CreateProtobufNetObject()
+        {
+            object jane = new PersonFromProtobufNet
+            {
+                Id = 321789,
+                Name = "Jane Doe",
+                Email = "jane.doe@example.com",
+            };
+
+            return jane;
+        }
+        
+    }
+}
---
+++ src/KafkaFlow.UnitTests/Serializers/Resources/PersonFromProtobufNet.cs
@@ -0,0 +1,36 @@
+﻿namespace KafkaFlow.UnitTests.Serializers.Resources
+{
+    using System;
+    using ProtoBuf;
+
+    [ProtoContract]
+    public class PersonFromProtobufNet
+    {
+        [ProtoMember(1)]
+        public int Id { get; set; }
+        
+        [ProtoMember(2)]
+        public string Name { get; set; }
+        
+        [ProtoMember((3))]
+        public string Email { get; set; }
+
+        protected bool Equals(PersonFromProtobufNet other)
+        {
+            return Id == other.Id && Name == other.Name && Email == other.Email;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((PersonFromProtobufNet) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id, Name, Email);
+        }
+    }
+}
