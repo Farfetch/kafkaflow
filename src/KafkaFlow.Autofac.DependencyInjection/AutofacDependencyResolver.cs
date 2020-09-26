173f04ce89cf0875c9c46ce2f34bbe68a1662273
paritosh baghel <paritoshmmmec@gmail.com>
2020-09-26 18:27:24 -0400

    feat(container): add support for autofac

---
+++ src/KafkaFlow.Autofac.DependencyInjection/AutofacDependencyResolver.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Autofac;
+
+namespace KafkaFlow.Autofac.DependencyInjection.KafkaFlow.Autofac
+{
+    /// <summary>
+    /// Autofac implementation of <see cref="IDependencyResolver"/>
+    /// </summary>
+    public class AutofacDependencyResolver : IDependencyResolver
+    {
+        private readonly ILifetimeScope scope;
+
+        /// <summary>
+        /// Creates a <see cref="AutofacDependencyResolver"/> instance
+        /// </summary>
+        /// <param name="scope"></param>
+        public AutofacDependencyResolver(ILifetimeScope scope) => this.scope = scope;
+
+        /// <inheritdoc />
+        public IDependencyResolverScope CreateScope()
+        {
+            return new AutofacDependencyResolverScope(this.scope.BeginLifetimeScope());
+        }
+
+        /// <inheritdoc />
+        public object Resolve(Type type) => this.scope.Resolve(type);
+    }
+}
---
+++ src/KafkaFlow.Autofac.DependencyInjection/AutofacDependencyResolverScope.cs
@@ -0,0 +1,20 @@
+﻿using Autofac;
+using KafkaFlow.Autofac.DependencyInjection.KafkaFlow.Autofac;
+
+namespace KafkaFlow.Autofac.DependencyInjection
+{
+    internal class AutofacDependencyResolverScope : IDependencyResolverScope
+    {
+        private readonly ILifetimeScope scope;
+
+        public AutofacDependencyResolverScope(ILifetimeScope scope)
+        {
+            this.scope = scope;
+            this.Resolver = new AutofacDependencyResolver(scope);
+        }
+
+        public void Dispose() => this.scope.Dispose();
+
+        public IDependencyResolver Resolver { get; }
+    }
+}
