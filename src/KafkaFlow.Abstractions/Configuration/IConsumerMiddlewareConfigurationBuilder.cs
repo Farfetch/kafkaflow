2a2de8a95f894b5c80cb6a8b35c496c74d3133c9
Joel Oliveira <joelfilipeoliveira@gmail.com>
2020-10-14 15:25:39 +0100

    feat: allow middlewares to be registered at the beginning

--- src/KafkaFlow.Abstractions/Configuration/IConsumerMiddlewareConfigurationBuilder.cs
+++ src/KafkaFlow.Abstractions/Configuration/IConsumerMiddlewareConfigurationBuilder.cs
@@ -20,11 +20,30 @@ namespace KafkaFlow.Configuration
             where T : class, IMessageMiddleware;
 
         /// <summary>
+        /// Registers a middleware at the beginning of the middleware list
+        /// The middleware will run before other middlewares that already have been registered
+        /// </summary>
+        /// <param name="factory">A factory to create the instance</param>
+        /// <typeparam name="T">A class that implements the <see cref="IMessageMiddleware"/></typeparam>
+        /// <returns></returns>
+        IConsumerMiddlewareConfigurationBuilder AddAtBeginning<T>(Factory<T> factory)
+            where T : class, IMessageMiddleware;
+
+        /// <summary>
         /// Registers a middleware
         /// </summary>
         /// <typeparam name="T">A class that implements the <see cref="IMessageMiddleware"/></typeparam>
         /// <returns></returns>
         IConsumerMiddlewareConfigurationBuilder Add<T>()
+            where T : class, IMessageMiddleware;
+
+        /// <summary>
+        /// Registers a middleware at the beginning of the middleware list 
+        /// The middleware will run before other middlewares that already have been registered
+        /// </summary>
+        /// <typeparam name="T">A class that implements the <see cref="IMessageMiddleware"/></typeparam>
+        /// <returns></returns>
+        IConsumerMiddlewareConfigurationBuilder AddAtBeginning<T>()
             where T : class, IMessageMiddleware;
     }
 }
