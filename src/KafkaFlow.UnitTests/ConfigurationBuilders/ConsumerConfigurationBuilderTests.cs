c0f54142ba3a7568ccebdd23b35cbfcf73df8fe3
Filipe Esch <filipe.esch@farfetch.com>
2021-02-17 10:15:02 +0000

    test: adds tests to consumer and producer builders

---
+++ src/KafkaFlow.UnitTests/ConfigurationBuilders/ConsumerConfigurationBuilderTests.cs
@@ -0,0 +1,126 @@
+namespace KafkaFlow.UnitTests.ConfigurationBuilders
+{
+    using System;
+    using AutoFixture;
+    using Confluent.Kafka;
+    using FluentAssertions;
+    using KafkaFlow.Configuration;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Moq;
+    using AutoOffsetReset = KafkaFlow.AutoOffsetReset;
+
+    [TestClass]
+    public class ConsumerConfigurationBuilderTests
+    {
+        private readonly Fixture fixture = new Fixture();
+
+        private Mock<IDependencyConfigurator> dependencyConfiguratorMock;
+
+        private ConsumerConfigurationBuilder target;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            this.dependencyConfiguratorMock = new Mock<IDependencyConfigurator>();
+
+            this.target = new ConsumerConfigurationBuilder(this.dependencyConfiguratorMock.Object);
+        }
+
+        [TestMethod]
+        public void DependencyConfigurator_SetProperty_ReturnPassedInstance()
+        {
+            // Assert
+            this.target.DependencyConfigurator.Should().Be(this.dependencyConfiguratorMock.Object);
+        }
+
+        [TestMethod]
+        public void Build_RequiredCalls_ReturnDefaultValues()
+        {
+            // Arrange
+            var clusterConfiguration = this.fixture.Create<ClusterConfiguration>();
+            var topic1 = this.fixture.Create<string>();
+            const int bufferSize = 100;
+            const int workers = 10;
+            var groupId = this.fixture.Create<string>();
+
+            this.target
+                .Topics(topic1)
+                .WithBufferSize(bufferSize)
+                .WithWorkersCount(workers)
+                .WithGroupId(groupId);
+
+            // Act
+            var configuration = this.target.Build(clusterConfiguration);
+
+            // Assert
+            configuration.Topics.Should().BeEquivalentTo(topic1);
+            configuration.BufferSize.Should().Be(bufferSize);
+            configuration.WorkerCount.Should().Be(workers);
+            configuration.GroupId.Should().Be(groupId);
+            configuration.GetKafkaConfig().AutoOffsetReset.Should().BeNull();
+            configuration.GetKafkaConfig().EnableAutoOffsetStore.Should().Be(false);
+            configuration.GetKafkaConfig().EnableAutoCommit.Should().Be(false);
+            configuration.AutoStoreOffsets.Should().Be(true);
+            configuration.AutoCommitInterval.Should().Be(TimeSpan.FromSeconds(5));
+            configuration.StatisticsHandlers.Should().BeEmpty();
+            configuration.MiddlewareConfiguration.Factories.Should().BeEmpty();
+        }
+
+        [TestMethod]
+        public void Build_AllCalls_ReturnPassedValues()
+        {
+            // Arrange
+            var clusterConfiguration = this.fixture.Create<ClusterConfiguration>();
+            var topic1 = this.fixture.Create<string>();
+            var topic2 = this.fixture.Create<string>();
+            var name = this.fixture.Create<string>();
+            const int bufferSize = 100;
+            const int workers = 10;
+            const AutoOffsetReset offsetReset = AutoOffsetReset.Earliest;
+            var groupId = this.fixture.Create<string>();
+            const int autoCommitInterval = 10000;
+            const int maxPollIntervalMs = 500000;
+            ConsumerCustomFactory customFactory = (producer, resolver) => producer;
+            Action<string> statisticsHandler = s => { };
+            const int statisticsIntervalMs = 100;
+            var consumerConfig = new ConsumerConfig();
+
+            this.target
+                .Topics(topic1)
+                .Topic(topic2)
+                .WithName(name)
+                .WithBufferSize(bufferSize)
+                .WithWorkersCount(workers)
+                .WithGroupId(groupId)
+                .WithAutoOffsetReset(offsetReset)
+                .WithManualStoreOffsets()
+                .WithAutoCommitIntervalMs(autoCommitInterval)
+                .WithMaxPollIntervalMs(maxPollIntervalMs)
+                .WithConsumerConfig(consumerConfig)
+                .WithCustomFactory(customFactory)
+                .WithStatisticsIntervalMs(statisticsIntervalMs)
+                .WithStatisticsHandler(statisticsHandler)
+                .AddMiddlewares(m => m.Add<IMessageMiddleware>());
+
+            // Act
+            var configuration = this.target.Build(clusterConfiguration);
+
+            // Assert
+            configuration.Topics.Should().BeEquivalentTo(topic1, topic2);
+            configuration.ConsumerName.Should().Be(name);
+            configuration.BufferSize.Should().Be(bufferSize);
+            configuration.WorkerCount.Should().Be(workers);
+            configuration.GroupId.Should().Be(groupId);
+            configuration.GetKafkaConfig().AutoOffsetReset.Should().Be(offsetReset);
+            configuration.AutoStoreOffsets.Should().Be(false);
+            configuration.GetKafkaConfig().EnableAutoOffsetStore.Should().Be(false);
+            configuration.GetKafkaConfig().EnableAutoCommit.Should().Be(false);
+            configuration.AutoCommitInterval.Should().Be(TimeSpan.FromMilliseconds(autoCommitInterval));
+            configuration.GetKafkaConfig().MaxPollIntervalMs.Should().Be(maxPollIntervalMs);
+            configuration.GetKafkaConfig().StatisticsIntervalMs.Should().Be(statisticsIntervalMs);
+            configuration.StatisticsHandlers.Should().HaveElementAt(0, statisticsHandler);
+            configuration.GetKafkaConfig().Should().BeSameAs(consumerConfig);
+            configuration.MiddlewareConfiguration.Factories.Should().HaveCount(1);
+        }
+    }
+}
