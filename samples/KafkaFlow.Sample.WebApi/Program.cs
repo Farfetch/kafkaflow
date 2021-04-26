e5535d29c5fbd196137bc0223600a8c340033656
Douglas Lima <douglas.lima@farfetch.com>
2021-04-26 13:37:25 -0300

    refactor: move webapi sample project to samples folder

--- samples/KafkaFlow.Sample.WebApi/Program.cs
+++ samples/KafkaFlow.Sample.WebApi/Program.cs
@@ -1,8 +1,8 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Hosting;
-
-namespace KafkaFlow.Admin.WebApi.Sample
+namespace KafkaFlow.Sample.WebApi
 {
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.Hosting;
+
     public static class Program
     {
         public static void Main(string[] args)
