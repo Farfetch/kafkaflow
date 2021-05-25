97602529918f268e507eb2cacb03509a97cb68ee
Douglas Lima <douglas.lima@farfetch.com>
2021-05-25 07:57:36 -0300

    feat: expose OnStop cluster handler

--- src/KafkaFlow/Configuration/KafkaConfiguration.cs
+++ src/KafkaFlow/Configuration/KafkaConfiguration.cs
@@ -1,10 +1,13 @@
 namespace KafkaFlow.Configuration
 {
+    using System;
     using System.Collections.Generic;
 
     internal class KafkaConfiguration
     {
         private readonly List<ClusterConfiguration> clusters = new();
+
+        public Action<IDependencyResolver> OnStopHandler = _ => { };
 
         public IReadOnlyCollection<ClusterConfiguration> Clusters => this.clusters;
 
