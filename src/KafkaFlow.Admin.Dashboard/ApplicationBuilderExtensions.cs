e83b90737255d5df3af34ba2ff6a554bca3dd406
Filipe Esch <filipe.esch@farfetch.com>
2021-06-14 16:06:47 +0100

    feat: changes how to serve client  app files

--- src/KafkaFlow.Admin.Dashboard/ApplicationBuilderExtensions.cs
+++ src/KafkaFlow.Admin.Dashboard/ApplicationBuilderExtensions.cs
@@ -1,6 +1,9 @@
 namespace KafkaFlow.Admin.Dashboard
 {
+    using System.Reflection;
     using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.Extensions.FileProviders;
 
     /// <summary>
     /// Extension methods over IApplicationBuilder
@@ -14,8 +17,25 @@ namespace KafkaFlow.Admin.Dashboard
         /// <returns></returns>
         public static IApplicationBuilder UseKafkaFlowDashboard(this IApplicationBuilder app)
         {
-            app
-                .UseMiddleware<ServeClientFilesMiddleware>();
+            app.Map(
+                "/kafka-flow",
+                builder =>
+                {
+                    var provider = new ManifestEmbeddedFileProvider(
+                        Assembly.GetAssembly(typeof(ApplicationBuilderExtensions)),
+                        "ClientApp/dist");
+
+                    builder.UseStaticFiles(new StaticFileOptions { FileProvider = provider });
+
+                    builder.Run(
+                        async context =>
+                        {
+                            if (context.Request.Path == "/")
+                            {
+                                await context.Response.SendFileAsync(provider.GetFileInfo("index.html"));
+                            }
+                        });
+                });
 
             return app;
         }
