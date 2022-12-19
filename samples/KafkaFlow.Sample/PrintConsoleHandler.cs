680cacae0563d46070163086ee319c08068f1c95
Guilherme Ferreira <guilherme.ferreira@farfetch.com>
2022-12-19 16:56:33 +0000

    chore: upgrade kafkaflow.sample to modern c# practices

---
+++ samples/KafkaFlow.Sample/PrintConsoleHandler.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Threading.Tasks;
+using KafkaFlow.TypedHandler;
+
+namespace KafkaFlow.Sample;
+
+public class PrintConsoleHandler : IMessageHandler<TestMessage>
+{
+    public Task Handle(IMessageContext context, TestMessage message)
+    {
+        Console.WriteLine(
+            "Partition: {0} | Offset: {1} | Message: {2}",
+            context.ConsumerContext.Partition,
+            context.ConsumerContext.Offset,
+            message.Text);
+
+        return Task.CompletedTask;
+    }
+}
