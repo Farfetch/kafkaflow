b04ede85840f73b8aad6cf252f12efd2b287c7fd
Guilherme Ferreira <guilherme.ferreira@farfetch.com>
2022-12-19 17:34:25 +0000

    chore: upgrade kafkaflow.sample.schemaregistry to modern c# practices

--- samples/KafkaFlow.Sample.SchemaRegistry/Handlers/AvroMessageHandler.cs
+++ samples/KafkaFlow.Sample.SchemaRegistry/Handlers/AvroMessageHandler.cs
@@ -1,21 +1,20 @@
-﻿namespace KafkaFlow.Sample.SchemaRegistry.Handlers
+﻿namespace KafkaFlow.Sample.SchemaRegistry.Handlers;
+
+using System;
+using System.Threading.Tasks;
+using global::SchemaRegistry;
+using TypedHandler;
+
+public class AvroMessageHandler : IMessageHandler<AvroLogMessage>
 {
-    using System;
-    using System.Threading.Tasks;
-    using global::SchemaRegistry;
-    using TypedHandler;
-
-    public class AvroMessageHandler : IMessageHandler<AvroLogMessage>
+    public Task Handle(IMessageContext context, AvroLogMessage message)
     {
-        public Task Handle(IMessageContext context, AvroLogMessage message)
-        {
-            Console.WriteLine(
-                "Partition: {0} | Offset: {1} | Message: {2} | Avro",
-                context.ConsumerContext.Partition,
-                context.ConsumerContext.Offset,
-                message.Severity.ToString());
+        Console.WriteLine(
+            "Partition: {0} | Offset: {1} | Message: {2} | Avro",
+            context.ConsumerContext.Partition,
+            context.ConsumerContext.Offset,
+            message.Severity.ToString());
 
-            return Task.CompletedTask;
-        }
+        return Task.CompletedTask;
     }
 }
--- samples/KafkaFlow.Sample.SchemaRegistry/Handlers/ProtobufMessageHandler.cs
+++ samples/KafkaFlow.Sample.SchemaRegistry/Handlers/ProtobufMessageHandler.cs
@@ -1,22 +1,21 @@
 ﻿
-namespace KafkaFlow.Sample.Avro.Handlers
+using System;
+using System.Threading.Tasks;
+using KafkaFlow.TypedHandler;
+using SchemaRegistry;
+
+namespace KafkaFlow.Sample.SchemaRegistry.Handlers;
+
+public class ProtobufMessageHandler : IMessageHandler<ProtobufLogMessage>
 {
-    using System;
-    using System.Threading.Tasks;
-    using global::SchemaRegistry;
-    using KafkaFlow.TypedHandler;
-
-    public class ProtobufMessageHandler : IMessageHandler<ProtobufLogMessage>
+    public Task Handle(IMessageContext context, ProtobufLogMessage message)
     {
-        public Task Handle(IMessageContext context, ProtobufLogMessage message)
-        {
-            Console.WriteLine(
-                "Partition: {0} | Offset: {1} | Message: {2} | Protobuf",
-                context.ConsumerContext.Partition,
-                context.ConsumerContext.Offset,
-                message.Message);
+        Console.WriteLine(
+            "Partition: {0} | Offset: {1} | Message: {2} | Protobuf",
+            context.ConsumerContext.Partition,
+            context.ConsumerContext.Offset,
+            message.Message);
 
-            return Task.CompletedTask;
-        }
+        return Task.CompletedTask;
     }
 }
