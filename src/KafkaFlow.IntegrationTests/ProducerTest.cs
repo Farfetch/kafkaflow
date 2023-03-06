b5fc9fc61576ed5798d6cadb31a39d97a6c6ea62
Guilherme Ferreira <guilherme.ferreira@farfetch.com>
2023-03-06 16:45:30 +0000

    fix: accept null as message key

---
+++ src/KafkaFlow.IntegrationTests/ProducerTest.cs
@@ -0,0 +1,41 @@
+namespace KafkaFlow.IntegrationTests
+{
+    using System;
+    using System.Threading.Tasks;
+    using AutoFixture;
+    using KafkaFlow.IntegrationTests.Core;
+    using KafkaFlow.IntegrationTests.Core.Handlers;
+    using KafkaFlow.IntegrationTests.Core.Producers;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class ProducerTest
+    {
+        private readonly Fixture fixture = new();
+
+        private IServiceProvider provider;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            this.provider = Bootstrapper.GetServiceProvider();
+            MessageStorage.Clear();
+        }
+
+
+        [TestMethod]
+        public async Task ProduceNullKeyTest()
+        {
+            // Arrange
+            var producer = this.provider.GetRequiredService<IMessageProducer<GzipProducer>>();
+            var message = this.fixture.Create<byte[]>();
+
+            // Act
+            await producer.ProduceAsync(null, message);
+
+            // Assert
+            await MessageStorage.AssertMessageAsync(message);
+        }
+    }
+}
