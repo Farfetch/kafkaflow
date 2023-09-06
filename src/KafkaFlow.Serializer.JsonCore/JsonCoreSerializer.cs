a30f81def298c39d8d692e5a51aa312952073c1d
Joel Oliveira <joel.oliveira@farfetch.com>
2023-09-06 11:36:09 +0100

    feat: add support for customizing JsonWriterOptions in JsonCoreSerializer

--- src/KafkaFlow.Serializer.JsonCore/JsonCoreSerializer.cs
+++ src/KafkaFlow.Serializer.JsonCore/JsonCoreSerializer.cs
@@ -10,7 +10,8 @@
     /// </summary>
     public class JsonCoreSerializer : ISerializer
     {
-        private readonly JsonSerializerOptions options;
+        private readonly JsonSerializerOptions serializerOptions;
+        private readonly JsonWriterOptions writerOptions;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="JsonCoreSerializer"/> class.
@@ -18,23 +19,43 @@
         /// <param name="options">Json serializer options</param>
         public JsonCoreSerializer(JsonSerializerOptions options)
         {
-            this.options = options;
+            this.serializerOptions = options;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="JsonCoreSerializer"/> class.
+        /// </summary>
+        /// <param name="writerOptions">Json writer options</param>
+        public JsonCoreSerializer(JsonWriterOptions writerOptions)
+        {
+            this.writerOptions = writerOptions;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="JsonCoreSerializer"/> class.
+        /// </summary>
+        /// <param name="serializerOptions">Json serializer options</param>
+        /// <param name="writerOptions">Json writer options</param>
+        public JsonCoreSerializer(JsonSerializerOptions serializerOptions, JsonWriterOptions writerOptions)
+        {
+            this.serializerOptions = serializerOptions;
+            this.writerOptions = writerOptions;
         }
 
         /// <summary>
         /// Initializes a new instance of the <see cref="JsonCoreSerializer"/> class.
         /// </summary>
         public JsonCoreSerializer()
-            : this(new JsonSerializerOptions())
+            : this(new JsonSerializerOptions(), default)
         {
         }
 
         /// <inheritdoc/>
         public Task SerializeAsync(object message, Stream output, ISerializerContext context)
         {
-            using var writer = new Utf8JsonWriter(output);
+            using var writer = new Utf8JsonWriter(output, this.writerOptions);
 
-            JsonSerializer.Serialize(writer, message, this.options);
+            JsonSerializer.Serialize(writer, message, this.serializerOptions);
 
             return Task.CompletedTask;
         }
@@ -43,7 +64,7 @@
         public async Task<object> DeserializeAsync(Stream input, Type type, ISerializerContext context)
         {
             return await JsonSerializer
-                .DeserializeAsync(input, type, this.options)
+                .DeserializeAsync(input, type, this.serializerOptions)
                 .ConfigureAwait(false);
         }
     }
