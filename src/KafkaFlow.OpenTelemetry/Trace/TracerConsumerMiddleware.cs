38b91aa8996465bf5c8d58e0a753ecba037ab752
Simão Ribeiro <simao.ribeiro@farfetch.com>
2023-09-13 17:07:32 +0100

    feat: creates kafkaflow.opentelemetry project
    Adds tracer middlwares for Producer and Consumer

---
+++ src/KafkaFlow.OpenTelemetry/Trace/TracerConsumerMiddleware.cs
@@ -0,0 +1,70 @@
+﻿namespace KafkaFlow.OpenTelemetry.Trace
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.Text;
+    using System.Threading.Tasks;
+    using global::OpenTelemetry;
+    using global::OpenTelemetry.Context.Propagation;
+
+    internal class TracerConsumerMiddleware : IMessageMiddleware
+    {
+        private static readonly TextMapPropagator Propagator = Propagators.DefaultTextMapPropagator;
+        private static readonly string ReceiveString = "receive";
+
+        public async Task Invoke(IMessageContext context, MiddlewareDelegate next)
+        {
+            var activityName = !string.IsNullOrEmpty(context?.ConsumerContext.Topic) ? $"{context.ConsumerContext.Topic} {ReceiveString}" : ReceiveString;
+
+            // Extract the PropagationContext of the upstream parent from the message headers.
+            var parentContext = Propagator.Extract(new PropagationContext(default, Baggage.Current), context, this.ExtractTraceContextIntoBasicProperties);
+            Baggage.Current = parentContext.Baggage;
+
+            // Start an activity with a name following the semantic convention of the OpenTelemetry messaging specification.
+            // The convention also defines a set of attributes (in .NET they are mapped as `tags`) to be populated in the activity.
+            // https://github.com/open-telemetry/opentelemetry-specification/blob/main/specification/trace/semantic_conventions/messaging.md
+            using var activity = KafkaFlowActivitySourceHelper.ActivitySource.StartActivity(activityName, ActivityKind.Consumer, parentContext.ActivityContext);
+
+            try
+            {
+                KafkaFlowActivitySourceHelper.SetGenericTags(activity);
+
+                if (activity != null && activity.IsAllDataRequested)
+                {
+                    this.SetConsumerTags(context, activity);
+                }
+
+                await next.Invoke(context);
+            }
+            catch (Exception ex)
+            {
+                activity?.SetTag("exception.message", ex.Message);
+
+                throw;
+            }
+        }
+
+        private IEnumerable<string> ExtractTraceContextIntoBasicProperties(IMessageContext context, string key)
+        {
+            try
+            {
+                return new[] { context.Headers.GetString(key, Encoding.UTF8) };
+            }
+            catch
+            {
+                return new string[] { };
+            }
+        }
+
+        private void SetConsumerTags(IMessageContext context, Activity activity)
+        {
+            activity.SetTag("messaging.operation", ReceiveString);
+            activity.SetTag("messaging.source.name", context.ConsumerContext.Topic);
+            activity.SetTag("messaging.kafka.consumer.group", context.ConsumerContext.GroupId);
+            activity.SetTag("messaging.kafka.message.key", context.Message.Key);
+            activity.SetTag("messaging.kafka.message.offset", context.ProducerContext.Offset);
+            activity.SetTag("messaging.kafka.source.partition", context.ConsumerContext.Partition);
+        }
+    }
+}
