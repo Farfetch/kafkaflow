b37e348b9d718f55ffa45a772e74759895bbba12
Simão Ribeiro <simao.ribeiro@farfetch.com>
2023-10-10 15:03:40 +0100

    Include some implementations

---
+++ src/KafkaFlow/GlobalEvents.cs
@@ -0,0 +1,36 @@
+﻿namespace KafkaFlow
+{
+    using System.Threading.Tasks;
+    using KafkaFlow.Configuration;
+
+    internal class GlobalEvents : IGlobalEvents
+    {
+        private readonly Event<MessageEventContext> messageConsumeStarted;
+
+        private readonly Event<MessageEventContext> messageConsumeStopped;
+
+        private readonly Event<MessageEventContext> messageProduceStarted;
+
+        public GlobalEvents(ILogHandler log)
+        {
+            this.messageConsumeStarted = new(log);
+            this.messageConsumeStopped = new(log);
+            this.messageProduceStarted = new(log);
+        }
+
+        public IEvent<MessageEventContext> MessageConsumeStarted => this.messageConsumeStarted;
+
+        public IEvent<MessageEventContext> MessageConsumeEnded => this.messageConsumeStopped;
+
+        public IEvent<MessageEventContext> MessageProduceStarted => this.messageProduceStarted;
+
+        public Task FireMessageConsumeStartedAsync(MessageEventContext context)
+            => this.messageConsumeStarted.FireAsync(context);
+
+        public Task FireMessageConsumeCompletedAsync(MessageEventContext context)
+            => this.messageConsumeStopped.FireAsync(context);
+
+        public Task FireMessageProduceStartedAsync(MessageEventContext context)
+            => this.messageProduceStarted.FireAsync(context);
+    }
+}
