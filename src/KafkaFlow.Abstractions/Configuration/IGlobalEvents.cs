c3f490ead0ef30043c0ecf0d1e7d79e174da40d4
Simão Ribeiro <simao.ribeiro@farfetch.com>
2023-10-10 14:59:27 +0100

    include abstractions

---
+++ src/KafkaFlow.Abstractions/Configuration/IGlobalEvents.cs
@@ -0,0 +1,23 @@
+﻿namespace KafkaFlow.Configuration
+{
+    /// <summary>
+    /// Provides access to events fired by the internals of the library
+    /// </summary>
+    public interface IGlobalEvents
+    {
+        /// <summary>
+        /// Gets the message consume started event
+        /// </summary>
+        IEvent<MessageEventContext> MessageConsumeStarted { get; }
+
+        /// <summary>
+        /// Gets the message consume ended event
+        /// </summary>
+        IEvent<MessageEventContext> MessageConsumeEnded { get; }
+
+        /// <summary>
+        /// Gets the message produce started event
+        /// </summary>
+        IEvent<MessageEventContext> MessageProduceStarted { get; }
+    }
+}
