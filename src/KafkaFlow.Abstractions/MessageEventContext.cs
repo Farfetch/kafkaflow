9d46d65611b192b9628a8fbfd6cd20ce871014e5
Simão Ribeiro <simao.ribeiro@farfetch.com>
2023-10-13 16:41:51 +0100

    code reviews

--- src/KafkaFlow.Abstractions/MessageEventContext.cs
+++ src/KafkaFlow.Abstractions/MessageEventContext.cs
@@ -5,20 +5,18 @@
     /// </summary>
     public class MessageEventContext
     {
-        private readonly IMessageContext messageContext;
-
         /// <summary>
         /// Initializes a new instance of the <see cref="MessageEventContext"/> class.
         /// </summary>
         /// <param name="messageContext">The message context</param>
         public MessageEventContext(IMessageContext messageContext)
         {
-            this.messageContext = messageContext;
+            this.MessageContext = messageContext;
         }
 
         /// <summary>
         /// Gets the message context
         /// </summary>
-        public IMessageContext MessageContext => this.messageContext;
+        public IMessageContext MessageContext { get; }
     }
 }
