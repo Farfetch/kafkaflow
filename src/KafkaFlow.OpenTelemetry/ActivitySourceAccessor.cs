f02fe4cbaa15aade949cea7ab3b2199cbd831063
Simão Ribeiro <simao.ribeiro@farfetch.com>
2023-10-13 14:48:52 +0100

    code review

--- src/KafkaFlow.OpenTelemetry/ActivitySourceAccessor.cs
+++ src/KafkaFlow.OpenTelemetry/ActivitySourceAccessor.cs
@@ -8,19 +8,22 @@ namespace KafkaFlow.OpenTelemetry
     using System.Reflection;
     using Conventions = SemanticConventions::OpenTelemetry.Trace.TraceSemanticConventions;
 
-    internal static class KafkaFlowActivitySourceHelper
+    internal static class ActivitySourceAccessor
     {
         internal const string ActivityString = "otel_activity";
-        internal const string ExceptionString = "exception";
-        internal const string KafkaString = "kafka";
-        internal static readonly AssemblyName AssemblyName = typeof(KafkaFlowActivitySourceHelper).Assembly.GetName();
+        internal const string ExceptionEventKey = "exception";
+        internal const string MessagingSystemId = "kafka";
+        internal const string AttributeMessagingOperation = "messaging.operation";
+        internal const string AttributeMessagingKafkaMessageKey = "messaging.kafka.message.key";
+        internal const string AttributeMessagingKafkaMessageOffset = "messaging.kafka.message.offset";
+        internal static readonly AssemblyName AssemblyName = typeof(ActivitySourceAccessor).Assembly.GetName();
         internal static readonly string ActivitySourceName = AssemblyName.Name;
         internal static readonly string Version = Assembly.GetExecutingAssembly().GetName().Version.ToString();
         internal static readonly ActivitySource ActivitySource = new(ActivitySourceName, Version);
 
         public static void SetGenericTags(Activity activity)
         {
-            activity?.SetTag(Conventions.AttributeMessagingSystem, KafkaString);
+            activity?.SetTag(Conventions.AttributeMessagingSystem, MessagingSystemId);
         }
 
         public static ActivityEvent CreateExceptionEvent(Exception exception)
@@ -32,7 +35,7 @@ namespace KafkaFlow.OpenTelemetry
                     new KeyValuePair<string, object>(Conventions.AttributeExceptionStacktrace, exception.StackTrace),
                 });
 
-            return new ActivityEvent(ExceptionString, DateTimeOffset.UtcNow, activityTagCollection);
+            return new ActivityEvent(ExceptionEventKey, DateTimeOffset.UtcNow, activityTagCollection);
         }
     }
 }
