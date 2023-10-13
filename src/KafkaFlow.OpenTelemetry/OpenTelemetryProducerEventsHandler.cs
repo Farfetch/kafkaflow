6dc3a3615872862340e11913f538d071f9c9690d
Simão Ribeiro <simao.ribeiro@farfetch.com>
2023-10-13 11:40:07 +0100

    codacy

--- src/KafkaFlow.OpenTelemetry/OpenTelemetryProducerEventsHandler.cs
+++ src/KafkaFlow.OpenTelemetry/OpenTelemetryProducerEventsHandler.cs
@@ -10,21 +10,21 @@
 
     internal static class OpenTelemetryProducerEventsHandler
     {
+        private const string PublishString = "publish";
         private static readonly TextMapPropagator Propagator = Propagators.DefaultTextMapPropagator;
-        private static readonly string PublishString = "publish";
 
         public static Task OnProducerStarted(IMessageContext context)
         {
             try
             {
-                var activityName = !string.IsNullOrEmpty(context?.ProducerContext.Topic) ? $"{context.ProducerContext.Topic} {PublishString}" : PublishString;
+                var activityName = !string.IsNullOrEmpty(context?.ProducerContext.Topic) ? $"{context?.ProducerContext.Topic} {PublishString}" : PublishString;
 
                 // Start an activity with a name following the semantic convention of the OpenTelemetry messaging specification.
                 // The convention also defines a set of attributes (in .NET they are mapped as `tags`) to be populated in the activity.
                 // https://github.com/open-telemetry/opentelemetry-specification/blob/main/specification/trace/semantic_conventions/messaging.md
                 var activity = KafkaFlowActivitySourceHelper.ActivitySource.StartActivity(activityName, ActivityKind.Producer);
 
-                context.Items.Add(KafkaFlowActivitySourceHelper.ActivityString, activity);
+                context?.Items.Add(KafkaFlowActivitySourceHelper.ActivityString, activity);
 
                 // Depending on Sampling (and whether a listener is registered or not), the
                 // activity above may not be created.
@@ -41,7 +41,7 @@
                     contextToInject = Activity.Current.Context;
                 }
 
-                Baggage.Current = Baggage.Create(activity.Baggage.ToDictionary(item => item.Key, item => item.Value));
+                Baggage.Current = Baggage.Create(activity?.Baggage.ToDictionary(item => item.Key, item => item.Value));
 
                 // Inject the ActivityContext into the message headers to propagate trace context to the receiving service.
                 Propagator.Inject(new PropagationContext(contextToInject, Baggage.Current), context, InjectTraceContextIntoBasicProperties);
@@ -55,6 +55,7 @@
             }
             catch
             {
+                // If there is any failure, do not propagate the context.
             }
 
             return Task.CompletedTask;
@@ -101,9 +102,9 @@
         {
             activity.SetTag("messaging.operation", PublishString);
             activity.SetTag("messaging.destination.name", context?.ProducerContext.Topic);
-            activity.SetTag("messaging.kafka.destination.partition", context.ProducerContext.Partition);
-            activity.SetTag("messaging.kafka.message.key", context.Message.Key);
-            activity.SetTag("messaging.kafka.message.offset", context.ProducerContext.Offset);
+            activity.SetTag("messaging.kafka.destination.partition", context?.ProducerContext.Partition);
+            activity.SetTag("messaging.kafka.message.key", context?.Message.Key);
+            activity.SetTag("messaging.kafka.message.offset", context?.ProducerContext.Offset);
         }
     }
 }
