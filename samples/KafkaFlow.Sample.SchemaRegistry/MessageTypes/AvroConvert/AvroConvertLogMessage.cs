7ea830d5c0ab2d80047a9ee958cea6a2e456f8b7
Robert Coltheart <robert.coltheart@gmail.com>
2023-12-18 11:32:37 +1100

    Tidying

--- samples/KafkaFlow.Sample.SchemaRegistry/MessageTypes/AvroConvert/AvroConvertLogMessage.cs
+++ samples/KafkaFlow.Sample.SchemaRegistry/MessageTypes/AvroConvert/AvroConvertLogMessage.cs
@@ -1,8 +1,13 @@
-﻿namespace SchemaRegistry;
+﻿using System.ComponentModel;
+using System.Runtime.Serialization;
 
+namespace SchemaRegistry;
+
+[DataContract(Name = nameof(AvroConvertLogMessage), Namespace = "SchemaRegistry")]
 public class AvroConvertLogMessage
 {
     public string Message { get; set; }
 
+    [DefaultValue(0)]
     public int Code { get; set; }
 }
