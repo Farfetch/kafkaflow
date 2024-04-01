c37f0da9bdd14d231ddf365dcfaa90589a145395
Joel Oliveira <joel.oliveira@farfetch.com>
2024-04-01 16:14:50 +0100

    fix: commit lint config file converted to .mjs

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -1,7 +1,7 @@
-const Configuration = {
+
+export default {
   extends: ['@commitlint/config-conventional'],
   rules: {
     'body-max-line-length': [0, 'always'],
-  },
-};
-module.exports = Configuration;
+  }  
+}
