935eae4c118bd538866c94710afc9bac19bbe4c4
francisco.dasilva <francisco.dasilva@farfetch.com>
2024-04-01 16:21:26 +0100

    fix: change extension to mjs to resolve error

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -1,7 +1,6 @@
-const Configuration = {
+export default {
   extends: ['@commitlint/config-conventional'],
   rules: {
     'body-max-line-length': [0, 'always'],
-  },
-};
-module.exports = Configuration;
+  }  
+}
