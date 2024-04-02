cf2524600a4877dc12a719b40edd8cf05ce66b8c
francisco.dasilva <francisco.dasilva@farfetch.com>
2024-04-02 08:06:19 +0100

    fix: fix to ignore header max length

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -1,5 +1,6 @@
 export default {
   extends: ['@commitlint/config-conventional'],
+  ignores: [(message) => /^Bumps \[.+]\(.+\) from .+ to .+\.$/m.test(message)],
   rules: {
     'body-max-line-length': [0, 'always'],
   }  
