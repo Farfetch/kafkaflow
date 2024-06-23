d4c8a6d189bea39c902a3a4245d724c8ddd30b8b
vchirikov <vovanchig@gmail.com>
2024-06-23 12:30:47 +0300

    feat: add an ability to call IServiceProvider during configuration
    Closes: https://github.com/Farfetch/kafkaflow/issues/271

--- src/KafkaFlow.Microsoft.DependencyInjection/ServiceCollectionExtensions.cs
+++ src/KafkaFlow.Microsoft.DependencyInjection/ServiceCollectionExtensions.cs
@@ -25,4 +25,23 @@ public static class ServiceCollectionExtensions
 
         return services.AddSingleton(configurator);
     }
+
+    /// <summary>
+    /// Configures KafkaFlow
+    /// </summary>
+    /// <param name="services">Instance of <see cref="IServiceCollection"/></param>
+    /// <param name="configure">A handler to configure KafkaFlow</param>
+    /// <returns></returns>
+    public static IServiceCollection AddKafka(
+        this IServiceCollection services,
+        Action<IServiceProvider, IKafkaConfigurationBuilder> configure)
+    {
+        return services.AddSingleton(sp =>
+        {
+            var configurator = new KafkaFlowConfigurator(
+                new MicrosoftDependencyConfigurator(services),
+                (builder) => configure(sp, builder));
+            return configurator;
+        });
+    }
 }
